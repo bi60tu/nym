05c5f66c517d533ac50866265e701cfef221c2f9
bi60tu <157617328+bi60tu@users.noreply.github.com>
2024-01-24 15:11:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,5 @@ const marker = L.marker([40.75703,-73.98597]).addTo(map)
 const marker2 = L.marker([40.74214, -73.98807]).addTo(map)
     .bindPopup('<b>Madison Square Park</b><br />A great park to walk around.');
 
+const marker = L.marker([40.7594005,-73.9736850]).addTo(map)
+		.bindPopup('<b>Tennis</b><br />.').openPopup();
